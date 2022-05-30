a8932072919b310d4191371e89df492d775660d2
Aishwarya Kashid <aishwaryakashid@Administrators-MacBook-Pro-4.local>
2022-05-30 15:11:25 +0530

    Add test cases for prime factors

--- src/Dummy.ts
+++ src/Dummy.ts
@@ -1,9 +1,13 @@
 export const dummyOutput = (input: number) => {
     const primeFactors : number[] = []
      
-    if(input / 1 === 1) {
+    if(input % 2 === 0) {
         // [...primeFactors, input]
-        primeFactors.push(input)
+        primeFactors.push(2)
+    }
+
+    if(input % 3 === 0) {
+        primeFactors.push(3)
     }
     console.log(primeFactors)
     return primeFactors;
--- test/src/Dummy.test.ts
+++ test/src/Dummy.test.ts
@@ -1,13 +1,14 @@
 import { dummyOutput } from '../../src/Dummy'
 
 describe("Dummy Test", () => {
-    // it("should assert on the dummy output", () => {
-    //     const dummyOutputReturned = dummyOutput("hello")
-    //     expect(dummyOutputReturned).toBe("hellohello")
-    // })
 
-    it("should return prime factors of number 1 as [1]", () => {
-        const dummyOutputReturned = dummyOutput(1)
-        expect(dummyOutputReturned).toEqual([1])
+    it("should return 2 as a prime factor of 2", () => {
+        const dummyOutputReturned = dummyOutput(2)
+        expect(dummyOutputReturned).toEqual([2])
+    })
+
+    it("should return 3 as a prime factor of 3", () => {
+        const dummyOutputReturned = dummyOutput(3)
+        expect(dummyOutputReturned).toEqual([3])
     })
 })
